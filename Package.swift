bb1582b34b60e19a41f0518af4b943d3f264295e
Mohamed Fouad <mohamed@fouad.me>
2022-06-12 10:20:34 +0200

    Fix package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,32 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
     name: "AlamofireXMLRPC",
+    platforms: [
+        .iOS(.v14),
+        .tvOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "AlamofireXMLRPC",
+            targets: ["AlamofireXMLRPC"]
+        )
+    ],
     dependencies: [
-        .Package(url: "https://github.com/Alamofire/Alamofire", majorVersion: 5),
-        .Package(url: "https://github.com/tadija/AEXML.git", majorVersion: 4)
+        .package(url: "https://github.com/Alamofire/Alamofire",.upToNextMajor(from: "5.6.1")),
+        .package(url: "https://github.com/tadija/AEXML.git", .upToNextMajor(from: "4.6.1"))
+    ],
+    targets: [
+        .target(
+            name: "AlamofireXMLRPC",
+            dependencies: [
+                "Alamofire",
+                "AEXML"
+            ],
+            exclude: [
+                "Info.plist"
+            ]
+        )
     ]
 )
